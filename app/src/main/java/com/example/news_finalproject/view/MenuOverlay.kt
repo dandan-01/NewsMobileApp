fc9ba40fbe08407d7cea3554cb332b3ccf209ad5
Dani <sanchezdanilyn@gmail.com>
2024-04-08 12:19:56 -0500

    Changed MenuOverlay to use ModalNavigationDrawer + ModalDrawerSheet

--- app/src/main/java/com/example/news_finalproject/view/MenuOverlay.kt
+++ app/src/main/java/com/example/news_finalproject/view/MenuOverlay.kt
@@ -45,7 +45,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.ModalDrawer
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.news_finalproject.ui.theme.News_FinalProjectTheme
 import com.example.news_finalproject.components.NavItem
@@ -53,88 +66,62 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun MenuOverlay(navController: NavController) {
-    @OptIn(ExperimentalMaterial3Api::class)
-    Column {
-        News_FinalProjectTheme {
-            val items = listOf(
-                NavItem(
-                    title = "Home",
-                    selectedIcon = Icons.Filled.Home,
-                    unselectedIcon = Icons.Outlined.Home
-                ),
-                NavItem(
-                    title = "Account",
-                    selectedIcon = Icons.Filled.AccountCircle,
-                    unselectedIcon = Icons.Outlined.AccountCircle
-                ),
-                NavItem(
-                    title = "Search",
-                    selectedIcon = Icons.Filled.Search,
-                    unselectedIcon = Icons.Outlined.Search
-                ),
-                NavItem(
-                    title = "Settings",
-                    selectedIcon = Icons.Filled.Settings,
-                    unselectedIcon = Icons.Outlined.Settings
-                )
-            )
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+    val coroutineScope = rememberCoroutineScope()
 
-            // surface
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                //
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-
-                // coroutine
-                val coroutineScope = rememberCoroutineScope()
-
-                //
-                var selectedItemIndex by rememberSaveable {
-                    mutableStateOf(0)
-                }
-
-                // Drawer = background overlay
-                ModalNavigationDrawer(
-                    drawerContent = {
-                        ModalDrawerSheet {
-                            Spacer(modifier = Modifier.height(16.dp))
-                            items.forEachIndexed { index, item ->
-                                NavigationDrawerItem(
-                                    label = {
-                                        Text(text = item.title)
-                                    },
-                                    selected = index == selectedItemIndex,
-                                    onClick = {
-                                        selectedItemIndex = index
-                                        coroutineScope.launch {
-                                            drawerState.close()
-                                        }
-                                    },
-                                    icon = {
-                                        Icon(
-                                            imageVector = if (index == selectedItemIndex) {
-                                                item.selectedIcon
-                                            } else item.unselectedIcon,
-                                            contentDescription = item.title
-                                        )
-                                    },
-                                    modifier = Modifier
-                                        .padding(NavigationDrawerItemDefaults.ItemPadding)
-                                        .clickable { // Add clickable modifier
-                                            selectedItemIndex = index
-                                            coroutineScope.launch {
-                                                drawerState.close()
-                                            }
-                                        }
-                                )
-                            }
-                        }
-                    },
-                    drawerState = drawerState
-                ) { }
-            }
-        }
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
     }
+
+    val items = listOf(
+        NavItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home
+        ),
+        NavItem(
+            title = "Account",
+            selectedIcon = Icons.Filled.AccountCircle,
+            unselectedIcon = Icons.Outlined.AccountCircle
+        )
+    )
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Spacer(modifier = Modifier.height(16.dp))
+                items.forEachIndexed { index, item ->
+                    NavigationDrawerItem(
+                        label = {
+                            Text(text = item.title)
+                        },
+                        selected = index == selectedItemIndex,
+                        onClick = {
+                            selectedItemIndex = index
+                            coroutineScope.launch {
+                                drawerState.close()
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = if (index == selectedItemIndex) {
+                                    item.selectedIcon
+                                } else item.unselectedIcon,
+                                contentDescription = item.title
+                            )
+                        },
+                        modifier = Modifier
+                            .padding(NavigationDrawerItemDefaults.ItemPadding)
+                            .clickable { // Add clickable modifier
+                                selectedItemIndex = index
+                                coroutineScope.launch {
+                                    drawerState.close()
+                                }
+                            }
+                    )
+                }
+            }
+        },
+        drawerState = drawerState
+    ) { }
 }
